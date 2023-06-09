fbd63e0149323eecb2aaee005a8c06fdfcff90e2
Abhishek12345679 <sah755146@gmail.com>
2023-06-09 22:29:24 +0530

    successfully displayed temp via converting json to actual class

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'dart:io';
+
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
+import 'package:weather_app_provider/models/weather_type.dart';
 
 class HomePage extends StatefulWidget {
   final Position? location;
@@ -16,12 +17,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<dynamic> getWeather() async {
+  Future<WeatherType?> getWeather() async {
     final location = widget.location;
     final client = http.Client();
     final apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
     final url = Uri.parse(
-        "https://api.openweathermap.org/data/3.0/onecall?lat=${location?.latitude}&lon=${location?.longitude}&exclude={part}&appid=$apiKey");
+      "https://api.openweathermap.org/data/3.0/onecall?lat=${location?.latitude}&lon=${location?.longitude}&exclude=minutely,hourly,daily,alerts&appid=$apiKey&units=metric",
+    );
 
     final response = await client.get(url);
 
@@ -29,7 +31,10 @@ class _HomePageState extends State<HomePage> {
 
     log('current weather: $currentWeatherJSON');
 
-    return currentWeatherJSON;
+    // if (currentWeatherJSON != null) {
+    final weather = WeatherType.fromMap(currentWeatherJSON);
+    return weather;
+    // }
   }
 
   @override
@@ -37,13 +42,53 @@ class _HomePageState extends State<HomePage> {
     return FutureBuilder(
       future: getWeather(),
       builder: (context, snapshot) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Today\'s Weather'),
-          ),
-          body: Text(
-              'lat: ${widget.location != null ? widget.location?.timestamp : ""}'),
-        );
+        final currentWeather = snapshot.data;
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            log(currentWeather.toString());
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Today\'s Weather'),
+              ),
+              body: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    height: 300,
+                    width: double.maxFinite,
+                    child: Column(children: [
+                      Container(
+                        height: 200,
+                        width: double.maxFinite,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Temperature",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 25,
+                            ),
+                          ),
+                          Text(
+                            '${currentWeather?.current.temp} C',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      )
+                    ]),
+                  ),
+                ],
+              ),
+            );
+
+          default:
+            return Center(child: CircularProgressIndicator());
+        }
       },
     );
   }
---
+++ lib/models/weather_type.dart
@@ -0,0 +1,166 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class WeatherType {
+  final double lat;
+  final double lon;
+  final String timezone;
+  final int timezoneOffset;
+  final CurrentWeather current;
+
+  const WeatherType({
+    required this.lat,
+    required this.lon,
+    required this.timezone,
+    required this.timezoneOffset,
+    required this.current,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'lat': lat,
+      'lon': lon,
+      'timezone': timezone,
+      'timezoneOffset': timezoneOffset,
+      'current': current.toMap(),
+    };
+  }
+
+  factory WeatherType.fromMap(Map<String, dynamic> map) {
+    return WeatherType(
+      lat: map['lat'] as double,
+      lon: map['lon'] as double,
+      timezone: map['timezone'] as String,
+      timezoneOffset: map['timezone_offset'] as int,
+      current: CurrentWeather.fromMap(map['current'] as Map<String, dynamic>),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory WeatherType.fromJson(String source) =>
+      WeatherType.fromMap(json.decode(source) as Map<String, dynamic>);
+}
+
+class CurrentWeather {
+  final int dt;
+  final int sunrise;
+  final int sunset;
+  final double temp;
+  final double feelsLike;
+  final int pressure;
+  final int humidity;
+  final double dewPoint;
+  final double uvi;
+  final int clouds;
+  final int visibility;
+  final double windSpeed;
+  final int windDeg;
+  // final double? windGust;
+  // final List<WeatherList>? weather;
+
+  const CurrentWeather({
+    required this.dt,
+    required this.sunrise,
+    required this.sunset,
+    required this.temp,
+    required this.feelsLike,
+    required this.pressure,
+    required this.humidity,
+    required this.dewPoint,
+    required this.uvi,
+    required this.clouds,
+    required this.visibility,
+    required this.windSpeed,
+    required this.windDeg,
+    // this.windGust,
+    // required this.weather,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'dt': dt,
+      'sunrise': sunrise,
+      'sunset': sunset,
+      'temp': temp,
+      'feelsLike': feelsLike,
+      'pressure': pressure,
+      'humidity': humidity,
+      'dewPoint': dewPoint,
+      'uvi': uvi,
+      'clouds': clouds,
+      'visibility': visibility,
+      'windSpeed': windSpeed,
+      'windDeg': windDeg,
+      // 'weather': weather?.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory CurrentWeather.fromMap(Map<String, dynamic> map) {
+    return CurrentWeather(
+      dt: map['dt'] as int,
+      sunrise: map['sunrise'] as int,
+      sunset: map['sunset'] as int,
+      temp: map['temp'] as double,
+      feelsLike: map['feels_like'] as double,
+      pressure: map['pressure'] as int,
+      humidity: map['humidity'] as int,
+      dewPoint: map['dew_point'] as double,
+      uvi: map['uvi'] as double,
+      clouds: map['clouds'] as int,
+      visibility: map['visibility'] as int,
+      windSpeed: map['wind_speed'] as double,
+      windDeg: map['wind_deg'] as int,
+      // weather: map['weather'] != null
+      //     ? List<WeatherList>.from(
+      //         (map['weather'] as List<int>).map<WeatherList?>(
+      //           (x) => WeatherList.fromMap(x as Map<String, dynamic>),
+      //         ),
+      //       )
+      //     : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CurrentWeather.fromJson(String source) =>
+      CurrentWeather.fromMap(json.decode(source) as Map<String, dynamic>);
+}
+
+class WeatherList {
+  final int? id;
+  final String? main;
+  final String? description;
+  final String? icon;
+
+  const WeatherList({
+    this.id,
+    this.main,
+    this.description,
+    this.icon,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'main': main,
+      'description': description,
+      'icon': icon,
+    };
+  }
+
+  factory WeatherList.fromMap(Map<String, dynamic> map) {
+    return WeatherList(
+      id: map['id'] != null ? map['id'] as int : null,
+      main: map['main'] != null ? map['main'] as String : null,
+      description:
+          map['description'] != null ? map['description'] as String : null,
+      icon: map['icon'] != null ? map['icon'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory WeatherList.fromJson(String source) =>
+      WeatherList.fromMap(json.decode(source) as Map<String, dynamic>);
+}
