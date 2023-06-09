2a34849922b6caee6e7afdbb7140e6a115e88172
Abhishek12345679 <sah755146@gmail.com>
2023-06-09 19:32:48 +0530

    successfully got location and wewather back and also learnt the differences between react and flutter (FutureBuilder vs lifestyle )

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,50 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  final Position? location;
+  const HomePage({super.key, this.location});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<dynamic> getWeather() async {
+    final location = widget.location;
+    final client = http.Client();
+    final apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
+    final url = Uri.parse(
+        "https://api.openweathermap.org/data/3.0/onecall?lat=${location?.latitude}&lon=${location?.longitude}&exclude={part}&appid=$apiKey");
+
+    final response = await client.get(url);
+
+    final currentWeatherJSON = json.decode(response.body);
+
+    log('current weather: $currentWeatherJSON');
+
+    return currentWeatherJSON;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: const Text(''),
+    return FutureBuilder(
+      future: getWeather(),
+      builder: (context, snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Today\'s Weather'),
+          ),
+          body: Text(
+              'lat: ${widget.location != null ? widget.location?.timestamp : ""}'),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
 
 import 'home_page.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.',
+    );
+  }
+
+  return await Geolocator.getCurrentPosition();
+}
+
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  final location = await _determinePosition();
+  runApp(MyApp(currentLocation: location));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Position? currentLocation;
+  const MyApp({super.key, this.currentLocation});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(useMaterial3: true),
-      title: 'MiniWork',
-      home: const HomePage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => null,
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(useMaterial3: true),
+        title: 'MiniWork',
+        home: HomePage(location: currentLocation),
+      ),
     );
   }
 }
